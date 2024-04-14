3cf49244c2da5ff4e756b4ed4f61647a93f5ffc0
namansehwal <namansehwal@gmail.com>
2024-04-14 18:04:02 +0530

    Added new song release and monthly activity report tasks to celery schedule

--- Backend/run.py
+++ Backend/run.py
@@ -3,7 +3,7 @@ from model import db
 from cache import cache
 from celery import Celery, Task
 from celery.schedules import crontab
-from task import daily_reminders
+from task import daily_reminders, new_song_release, monthly_activity_report
 
 def celery_init_app(app):
     class FlaskTask(Task):
@@ -21,17 +21,19 @@ def celery_init_app(app):
     return celery_app
 celery_app = celery_init_app(app)
 
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(crontab(minute='*', hour='*'), daily_reminders.s()) 
+    sender.add_periodic_task(crontab(minute='*', hour='*'), new_song_release.s())
+    sender.add_periodic_task(crontab(minute='*', hour='*'), monthly_activity_report.s())
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         cache.init_app(app)
-        @celery_app.on_after_configure.connect
-        def setup_periodic_tasks(sender, **kwargs):
-            
-            # Reminding users to login
-            sender.add_periodic_task(crontab(minute='*', hour='*'), daily_reminders.s())
         
+            
+            
         app.run(debug=True)
         
--- Backend/task.py
+++ Backend/task.py
@@ -4,8 +4,11 @@ from mail_server import send_email
 from model import db, User, Song, Album
 from datetime import datetime, timedelta
 from sqlalchemy import and_
+from celery import shared_task
 
+@shared_task(ignore_result=True)
 def daily_reminders():
+    print("Daily reminder task started")
     # users = User.query.all()
     one_day_ago=datetime.now()-timedelta(days=1)
     users = User.query.filter(User.login_at>=one_day_ago).all()
@@ -13,6 +16,7 @@ def daily_reminders():
         reminder_webhook(user.username)
         print(f"Reminder sent to {user.username}")
 
+@shared_task(ignore_result=True)
 def new_song_release():
     one_day_ago=datetime.now()-timedelta(days=1)
     songs = Song.query.filter(Song.created_at<=one_day_ago).all()
@@ -20,14 +24,15 @@ def new_song_release():
         song_webhook(song.song_name)
         print(f'notification for song {song.song_name} sent')
 
+@shared_task(ignore_result=True)
 def monthly_activity_report():
     users=User.query.filter(User.status=='wlc').all()
     for creator in users:
         # creator=User.query.get(int(user_id))
         user_id=creator.id
         seven_days_ago=datetime.now()-timedelta(days=7)
-        songs = Song.query.filter(and_(Song.user_id == int(user_id), Song.created_on >= seven_days_ago)).all()
-        albums = Album.query.filter(and_(Album.user_id == int(user_id), Album.created_on >= seven_days_ago)).all()
+        songs = Song.query.filter(and_(Song.user_id == int(user_id), Song.created_at >= seven_days_ago)).all()
+        albums = Album.query.filter(and_(Album.user_id == int(user_id), Album.created_at >= seven_days_ago)).all()
 
         content_body = "<h1>Weekly Report</h1>"
         content_body += f"<h2>Creator: {creator.username}</h2>"
